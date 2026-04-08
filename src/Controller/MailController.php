118607ed66d15a88e0d6c9fc28917f4b2f509c8a
kerfiguetteb <kerfiguetteboutman@gmail.com>
2026-04-08 11:27:58 +0200

    feat:mailer CICD

---
+++ src/Controller/MailController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class MailController extends AbstractController
+{
+      #[Route('/send-mail', name: 'send_mail')]
+    public function sendMail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('demo@example.com')
+            ->to('test@exemple.com')
+            ->subject('Bonjour depuis Symfony !')
+            ->text('Ceci est un email de test.');
+
+        $mailer->send($email);
+
+        return new Response("✅ Email envoyé !");
+    }
+}
---
+++ tests/MailTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MailTest extends WebTestCase {
+     public function testMailEnvoi(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/send-mail');
+
+        // $this->assertResponseIsSuccessful();
+        $this->assertEmailCount(1); 
+    }
+}
